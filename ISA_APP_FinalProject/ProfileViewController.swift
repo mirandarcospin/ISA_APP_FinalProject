7a88ec92c5b75b5c5afe853cc0dca1a0d39a589f
Miranda Ramirez Cospin <mxr17e@acu.edu>
2020-11-18 22:42:07 -0600

    Profile works. Saves data :)

--- ISA_APP_FinalProject/ProfileViewController.swift
+++ ISA_APP_FinalProject/ProfileViewController.swift
@@ -15,6 +15,7 @@ class ProfileViewController: UIViewController {
     var fresult: Bool = false
     var subUrl: URL?
     var mainUrl: URL? = Bundle.main.url(forResource: "profile", withExtension: "json")
+    let darkGreyColor = UIColor(red: 94/255, green: 94/255, blue: 94/255, alpha: 1.0)
     
     
     @IBOutlet weak var profileImageView: UIImageView!
@@ -37,10 +38,6 @@ class ProfileViewController: UIViewController {
         
         title = "Profile"
 
-        // 1
-        let nav = self.navigationController?.navigationBar
-        nav?.barStyle = UIBarStyle.black
-
         let imageTap = UITapGestureRecognizer(target: self, action: #selector(openImagePicker))
         profileImageView.isUserInteractionEnabled = true
         profileImageView.addGestureRecognizer(imageTap)
@@ -56,6 +53,10 @@ class ProfileViewController: UIViewController {
         
         setupTexts()
         setupEditButton()
+    }
+    
+    @objc func openImagePicker(_ sender:Any){
+        //open image picker
     }
     
     func getData() {
@@ -89,10 +90,6 @@ class ProfileViewController: UIViewController {
         } catch {}
     }
     
-    @objc func openImagePicker(_ sender:Any){
-        //open image picker
-    }
-    
     func setupTexts() {
         nameLabel.text = profileData[0].name
         emailLabel.text = profileData[0].email
@@ -105,7 +102,7 @@ class ProfileViewController: UIViewController {
     }
     
     func validate() -> Bool{
-        if(nameLabel.text!.isEmpty || emailLabel.text!.isEmpty || collegeLabel.text!.isEmpty || roleLabel.text!.isEmpty || gradYearLabel.text!.isEmpty || birthdayLabel.text!.isEmpty || countryLabel.text!.isEmpty || instaLabel.text!.isEmpty){
+        if(nameLabel.text!.isEmpty || emailLabel.text!.isEmpty || collegeLabel.text!.isEmpty || roleLabel.text!.isEmpty || gradYearLabel.text!.isEmpty || birthdayLabel.text!.isEmpty || countryLabel.text!.isEmpty){
             return false
         }
         return true
@@ -122,6 +119,11 @@ class ProfileViewController: UIViewController {
     
     func setupEditButton(){
         let editButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(tappedEditButton))
+        editButton.setTitleTextAttributes(
+        [
+            NSAttributedString.Key.font : UIFont(name: "Avenir Next Condensed Regular", size: 20.0)!,
+            NSAttributedString.Key.foregroundColor : UIColor.white,
+        ], for: .normal)
         self.navigationItem.rightBarButtonItem = editButton
     }
     
@@ -143,31 +145,97 @@ class ProfileViewController: UIViewController {
         collegeLabel.borderStyle = .roundedRect
         collegeLabel.backgroundColor = .white
         collegeLabel.textColor = .black
+        
+        roleLabel.isUserInteractionEnabled = true
+        roleLabel.borderStyle = .roundedRect
+        roleLabel.backgroundColor = .white
+        roleLabel.textColor = .black
+        
+        gradYearLabel.isUserInteractionEnabled = true
+        gradYearLabel.borderStyle = .roundedRect
+        gradYearLabel.backgroundColor = .white
+        gradYearLabel.textColor = .black
+        
+        birthdayLabel.isUserInteractionEnabled = true
+        birthdayLabel.borderStyle = .roundedRect
+        birthdayLabel.backgroundColor = .white
+        birthdayLabel.textColor = .black
+        
+        countryLabel.isUserInteractionEnabled = true
+        countryLabel.borderStyle = .roundedRect
+        countryLabel.backgroundColor = .white
+        countryLabel.textColor = .black
+        
+        instaLabel.isUserInteractionEnabled = true
+        instaLabel.borderStyle = .roundedRect
+        instaLabel.backgroundColor = .white
+        instaLabel.textColor = .black
     }
     
     func setupSaveButton(){
         let saveButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(tappedSaveButton))
+        saveButton.setTitleTextAttributes(
+        [
+            NSAttributedString.Key.font : UIFont(name: "Avenir Next Condensed Regular", size: 20.0)!,
+            NSAttributedString.Key.foregroundColor : UIColor.white,
+        ], for: .normal)
         self.navigationItem.rightBarButtonItem = saveButton
     }
     
     @objc func tappedSaveButton() {
         
-        if validate() == true{
+        if validate() == true {
             
             profileData[0].name = nameLabel.text!
             profileData[0].email = emailLabel.text!
             profileData[0].college = collegeLabel.text!
+            profileData[0].role = roleLabel.text!
+            profileData[0].gradYear = gradYearLabel.text!
+            profileData[0].birthday = birthdayLabel.text!
+            profileData[0].country = countryLabel.text!
+            profileData[0].insta = instaLabel.text!
             
             setupEditButton()
             
             nameLabel.isUserInteractionEnabled = false
-            nameLabel.borderStyle = .none
+            nameLabel.borderStyle = .roundedRect
+            nameLabel.backgroundColor = darkGreyColor
+            nameLabel.textColor = .white
 
             emailLabel.isUserInteractionEnabled = false
-            emailLabel.borderStyle = .none
+            emailLabel.borderStyle = .roundedRect
+            emailLabel.backgroundColor = darkGreyColor
+            emailLabel.textColor = .white
             
             collegeLabel.isUserInteractionEnabled = false
-            collegeLabel.borderStyle = .none
+            collegeLabel.borderStyle = .roundedRect
+            collegeLabel.backgroundColor = darkGreyColor
+            collegeLabel.textColor = .white
+            
+            roleLabel.isUserInteractionEnabled = false
+            roleLabel.borderStyle = .roundedRect
+            roleLabel.backgroundColor = darkGreyColor
+            roleLabel.textColor = .white
+            
+            gradYearLabel.isUserInteractionEnabled = false
+            gradYearLabel.borderStyle = .roundedRect
+            gradYearLabel.backgroundColor = darkGreyColor
+            gradYearLabel.textColor = .white
+            
+            birthdayLabel.isUserInteractionEnabled = false
+            birthdayLabel.borderStyle = .roundedRect
+            birthdayLabel.backgroundColor = darkGreyColor
+            birthdayLabel.textColor = .white
+            
+            countryLabel.isUserInteractionEnabled = false
+            countryLabel.borderStyle = .roundedRect
+            countryLabel.backgroundColor = darkGreyColor
+            countryLabel.textColor = .white
+            
+            instaLabel.isUserInteractionEnabled = false
+            instaLabel.borderStyle = .roundedRect
+            instaLabel.backgroundColor = darkGreyColor
+            instaLabel.textColor = .white
             
             fresult = true
             
@@ -175,24 +243,12 @@ class ProfileViewController: UIViewController {
                 writeToFile(location: subUrl!)
                 fresult = false
             }
-        }else {
-            let alert = UIAlertController(title: "Reminder:", message: "All fields are required!", preferredStyle: .alert)
+
+        } else {
+            let alert = UIAlertController(title: "Reminder:", message: "All fields are required. Insatgram is optional", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
-        }
+       }
     }
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
